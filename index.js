f640e0533407652bde463a2f8befe169e5420a92
Prateek Saxena <prtksxna@gmail.com>
2018-07-11 15:34:43 +0530

    docs: add general and API documentation

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 'use strict';
 
-// TODO We use this only for debugging
-var logger = require('jsdoc/util/logger');
+// We use this only for debugging
+// var logger = require('jsdoc/util/logger');
 
 // TODO Get config
+// Should the HTML lists be added to the doclet description?
 var showList = true;
 
 // Key map of classes and what they inherit from:
 // { 'childClassName': 'parentClassName' }
 var parents = {};
 
-var findAllParents = function ( p ) {
+/**
+ * A recursive function that finds all the parents of a given class.
+ *
+ * @param {string[]} p A single element array with the name of the class
+ *                     whose parents we need to find
+ * @returns {string[]} An array of all parents starting with the highest
+ */
+function findAllParents( p ) {
 	var lastParent = p[ 0 ];
 	var lastParentsParent = parents[ lastParent ];
 	if ( lastParentsParent === undefined ) {
@@ -21,7 +29,13 @@ var findAllParents = function ( p ) {
 	}
 }
 
-var findDirectChildren = function ( className ) {
+/**
+ * Finds all the direct children of a given class name.
+ *
+ * @param {string} className
+ * @returns {string[]} Array of the child classes' names
+ */
+function findDirectChildren( className ) {
 	var children = [];
 	for ( var longname in parents ) {
 		if ( parents[ longname ] === className ) {
@@ -31,11 +45,24 @@ var findDirectChildren = function ( className ) {
 	return children;
 }
 
-var linkTo = function ( className ) {
+/**
+ * Creates a link to a given class.
+ *
+ * @param {string} className
+ * @returns {string} HTML string of '<a>'
+ */
+function linkTo( className ) {
 	return '<a href="' + className + '.html">' + className + '</a>';
 }
 
-var makeHierarchyList = function ( classes ) {
+/**
+ * Recursive function that creates a nested list of a class' parents.
+ *
+ *
+ * @param {string[]} classes Array of class' parent's names
+ * @returns {string} HTML string of a `<ul>`
+ */
+function makeHierarchyList( classes ) {
 	if ( classes.length === 0 ) {
 		return '';
 	} else {
@@ -44,7 +71,13 @@ var makeHierarchyList = function ( classes ) {
 	}
 }
 
-var makeChildrenList = function ( classes ) {
+/**
+ * Creates a list of child classes.
+ *
+ * @param {string[]} classes Array of all the child classes' names.
+ * @returns {string} HTML string of a `<ul>`
+ */
+function makeChildrenList( classes ) {
 	var list = '<ul>';
 	classes.forEach( function ( className ) {
 		list += '<li>' + linkTo( className ) + '</li>';
@@ -53,7 +86,9 @@ var makeChildrenList = function ( classes ) {
 	return list;
 }
 
+// Setup event handlers for JSDoc
 exports.handlers = {
+	// Add names to the parents map for every new doclet
 	newDoclet: function ( e ) {
 		var doclet = e.doclet;
 		if (
@@ -65,11 +100,13 @@ exports.handlers = {
 		}
 	},
 
+	// Once all doclets are processed, go through them and create hierarchy
+	// and children lists. Also add the HTML versions if the config allows.
 	processingComplete: function(e) {
 		// Traverse list and get all parents
 		e.doclets.forEach( function (d) {
       if ( d.kind === 'class'	) {
-				var list = '';
+				var html = '';
 
 				if (
 					d.augments !== undefined &&
@@ -77,19 +114,19 @@ exports.handlers = {
 				) {
 					d.hierarchy = findAllParents( [ d.longname ] );
 					if ( d.hierarchy.length > 0 && showList ) {
-						list += '<h3>Hierarchy</h3>' + makeHierarchyList( d.hierarchy.slice() );
+						html += '<h3>Hierarchy</h3>' + makeHierarchyList( d.hierarchy.slice() );
 					}
 				}
 
 				d.children = findDirectChildren( d.longname );
 				if ( d.children.length > 0 && showList ) {
-					list += '<h3>Children</h3>' + makeChildrenList( d.children.slice() );
+					html += '<h3>Children</h3>' + makeChildrenList( d.children.slice() );
 				}
 
 				d.description = d.description === undefined ? '' : d.description;
 				// Otherwise, using += appends 'undefined' as a string
 
-				d.description = '<small>' + list + '</small>' + d.description;
+				d.description = '<small>' + html + '</small>' + d.description;
       }
     });
   }
